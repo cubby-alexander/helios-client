e36f2ec2c4f7e409d42f00e9d7aa018141cfb886
Jacob "Cubby" Hall <hall.j.alexander@gmail.com>
2024-12-12 17:26:21 -0500

    [feat] HELCLI-28: form context (#31)
    * Refactors navigation structure of landing/discovery
    
    * Adds form selection step to discovery flow
    
    * Adds initial project form fields
    
    * Changes style behavior

---
+++ src/app/interfaces/DiscoveryFlow/ProjectScopeForm.tsx
@@ -0,0 +1,148 @@
+import { Accordion, AccordionItem, ScrollShadow } from '@nextui-org/react';
+import { FunnelIcon, GlobeAmericasIcon, WrenchIcon } from '@heroicons/react/16/solid';
+// @ts-ignore
+import styles from '../../discover/page.module.css';
+import { RefObject, useRef, useState } from 'react';
+import { OpsQuestionList, OrgOpsList, RefinedOpsList } from '../../types/DiscoveryFormTypes';
+import OpsRefineSection from './OpsRefineSection';
+import { SelectKeyOpsSection } from './SelectKeyOpsSection';
+import { DiscoverySection } from './DiscoverySection';
+import { mockOpsQuestionList } from '../../mocks/ops-question-list';
+import { DISCOVERY_CONTENT } from '../../enums';
+import { ArrowsPointingInIcon } from '@heroicons/react/24/outline';
+import { ProjectGoalSection } from './ProjectGoalSection';
+
+interface ProjectScopeFormProps {
+  setDiscovery: (content: DISCOVERY_CONTENT) => void;
+}
+
+export default function ProjectScopeForm({ setDiscovery }: ProjectScopeFormProps) {
+  const [openKeys, setOpenKeys] = useState<string[]>(['1']);
+  const [disabledKeys, setDisabledKeys] = useState<string[]>(['2', '3', '4']);
+  const [projectGoal, setProjectGoal] = useState<string>('');
+  const [orgOpsList, setOrgOpsList] = useState<OrgOpsList | null>(null);
+  const [refinedOpsList, setRefinedOpsList] = useState<RefinedOpsList | null>(null);
+  const [opsQuestionList, setOpsQuestionList] = useState<OpsQuestionList | null>(
+    mockOpsQuestionList
+  );
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const refineRef = useRef<HTMLDivElement | null>(null);
+  const filterRef = useRef<HTMLDivElement | null>(null);
+  const analyzeRef = useRef<HTMLDivElement | null>(null);
+
+  const itemClasses = {
+    base: 'w-full',
+    title: 'font-normal text-slate-200 pl-4',
+    subtitle: 'pl-4 text-small',
+    content: 'text-small px-2 text-xs'
+  };
+
+  const handleScroll = (element: RefObject<HTMLDivElement>) => {
+    if (element.current) {
+      element.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className='relative w-screen h-[70vh] flex flex-col justify-start items-center text-left z-20'>
+      <div className={`text-gray-400 font-extralight text-3xl mb-4`}>
+        Satellite Services For A Project
+      </div>
+
+      <ScrollShadow className='relative w-screen h-[70vh] flex flex-col justify-start items-center text-left my-2 z-20'>
+        <Accordion
+          className='p-2 flex flex-col gap-1 w-[80vw] max-w-[800px]'
+          itemClasses={itemClasses}
+          showDivider={false}
+          disableAnimation={false}
+          disableIndicatorAnimation={false}
+          hideIndicator={true}
+          isCompact={false}
+          isDisabled={false}
+          defaultExpandedKeys={['1']}
+          disabledKeys={disabledKeys}
+          selectedKeys={openKeys}
+        >
+          <AccordionItem
+            key='1'
+            aria-label='Project Goals and Context'
+            startContent={<ArrowsPointingInIcon className='text-slate-400 h-7 w-7' />}
+            title={<p className='text-lg font-medium'>Project Goals and Context</p>}
+            subtitle="Tell us what you're trying to do"
+          >
+            <ProjectGoalSection
+              textRef={textareaRef}
+              projectGoalChange={(scope: string) => setProjectGoal(scope)}
+              orgOpsChange={(ops: OrgOpsList) => setOrgOpsList(ops)}
+              disabledKeyChange={setDisabledKeys}
+              openKeyChange={setOpenKeys}
+              scroll={{
+                handler: handleScroll,
+                target: refineRef
+              }}
+            />
+          </AccordionItem>
+          <AccordionItem
+            key='2'
+            aria-label='Project Team'
+            startContent={<WrenchIcon className='text-slate-400 h-7 w-7' />}
+            title={<p className='text-lg font-medium'>Project Team</p>}
+            subtitle='Confirm how you work'
+          >
+            <OpsRefineSection
+              orgOpsList={orgOpsList}
+              orgScope={projectGoal}
+              disabledKeyChange={setDisabledKeys}
+              openKeyChange={setOpenKeys}
+              refinedOpsListChange={setRefinedOpsList}
+              scrollRef={refineRef}
+              scroll={{
+                handler: handleScroll,
+                target: filterRef
+              }}
+            />
+          </AccordionItem>
+          <AccordionItem
+            key='3'
+            aria-label='Select Key Operations'
+            startContent={<FunnelIcon className='text-slate-400 h-7 w-7' />}
+            title={<p className='font-medium'>Select Key Operations</p>}
+            subtitle='What do you need?'
+          >
+            <SelectKeyOpsSection
+              refinedOpsList={refinedOpsList}
+              orgScope={projectGoal}
+              opsQuestionListChange={setOpsQuestionList}
+              disabledKeyChange={setDisabledKeys}
+              openKeyChange={setOpenKeys}
+              scrollRef={filterRef}
+              scroll={{
+                handler: handleScroll,
+                target: analyzeRef
+              }}
+            />
+          </AccordionItem>
+          <AccordionItem
+            key='4'
+            aria-label='Card expired'
+            startContent={<GlobeAmericasIcon className='text-slate-400 h-7 w-7' />}
+            subtitle='What unlocks value?'
+            title={<p className='font-medium'>Generate Satellite Solutions</p>}
+          >
+            <DiscoverySection
+              orgScope={projectGoal}
+              opsQuestionList={opsQuestionList}
+              scrollRef={analyzeRef}
+            />
+          </AccordionItem>
+        </Accordion>
+      </ScrollShadow>
+      <p
+        onClick={() => setDiscovery(DISCOVERY_CONTENT.SELECTION)}
+        className={`${styles.backnav} text-gray-300 font-normal w-[80vw] max-w-[800px] cursor-pointer`}
+      >
+        <span>&lt;-</span> Return to discovery selection (progress not saved)
+      </p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
 'use client';
-import LandingContent from './interfaces/LandingContent';
-import DiscoveryFlow from './interfaces/DiscoveryFlow/DiscoveryFlow';
-import { useState } from 'react';
-import { CONTENT } from './enums';
+import styles from '@/app/page.module.css';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const [content, setContent] = useState(CONTENT.LANDING);
+  const router = useRouter();
 
   return (
-    <div>
-      {content === CONTENT.LANDING && <LandingContent changeContent={setContent} />}
-      {content === CONTENT.DISCOVERY_FLOW && <DiscoveryFlow />}
+    <div className={`${styles.landing}`}>
+      <div className={styles.center}>
+        <span>
+          unlock space economy value in our
+          <h2>ORBITAL SERVICE MARKET</h2>
+        </span>
+      </div>
+
+      <div className={styles.grid}>
+        <div className={`${styles.card} cursor-pointer`} onClick={() => router.push('/discover')}>
+          <h2>
+            Discover Services <span>-&gt;</span>
+          </h2>
+          <p>Use AI to learn how satellite imaging can deliver value to your organization</p>
+        </div>
+
+        <div className={`${styles.card}`}>
+          <h2>Order Services</h2>
+          <p>Find mission operators who can provide your desired service. Coming soon...</p>
+        </div>
+      </div>
     </div>
   );
 }
